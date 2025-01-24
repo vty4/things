a290e325e56d56b2a0daf5c6593816a6ddcc944b
vty4 <tumarusakada124@gmail.com>
2025-01-24 11:32:35 +0700

    Update dh-hb-ep.lua

--- dh-hb-ep.lua
+++ dh-hb-ep.lua
@@ -1,7 +1,17 @@
--- // Ensure is Da Hood (put in autoexec)
+-- Ensure is Da Hood (put in autoexec)
 if (game.PlaceId ~= 2788229376) then
     return
 end
+
+-- Global settings
+getgenv().settings = {
+    Size = Vector3.new(5, 5, 5), -- Default hitbox size
+    Color = Color3.fromRGB(255, 0, 0), -- Red hitbox
+    Transparency = 0.5 -- Semi-transparent
+}
+
+-- Load external script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vty4/things/refs/heads/main/dh-hb-ep.lua"))()
 
 -- // Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -20,48 +30,33 @@ local Flags = {
 -- // __namecall hook
 local __namecall
 __namecall = hookmetamethod(game, "__namecall", function(...)
-    -- // Vars
     local args = {...}
     local self = args[1]
     local method = getnamecallmethod()
 
-    -- // See if the game is trying to alert the server
     if (method == "FireServer" and self == MainEvent and tablefind(Flags, args[2])) then
         return
     end
 
-    -- // Anti Crash
     if (not checkcaller() and getfenv(2).crash) then
-        -- // Set the crash function (hooking can cause stutters)
         local fenv = getfenv(2)
         fenv.crash = function() end
         setfenv(2, fenv)
     end
 
-    -- //
     return __namecall(...)
 end)
 
 -- // __newindex hook (stops game from setting ws/jp)
 local __newindex
 __newindex = hookmetamethod(game, "__newindex", function(t, k, v)
-    -- // Make sure it's trying to set our humanoid's ws/jp
     if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
-        -- // Disallow the set
         return
     end
-
-    -- //
     return __newindex(t, k, v)
 end)
 
 -- Hitbox Expander Script
-getgenv().settings = {
-    Size = Vector3.new(5, 5, 5), -- Default hitbox size
-    Color = Color3.fromRGB(255, 0, 0), -- Red hitbox
-    Transparency = 0.5 -- Semi-transparent
-}
-
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
@@ -95,7 +90,6 @@ end
 
 Players.PlayerAdded:Connect(function(player)
     player.CharacterAdded:Connect(function(character)
-        -- Retry expanding hitbox immediately after respawn
         if player ~= LocalPlayer then
             expandHitbox(character)
         end
@@ -108,7 +102,6 @@ Players.PlayerRemoving:Connect(function(player)
     end
 end)
 
--- Run the script every 3 seconds to apply hitbox expansion
 while true do
     expandAllHitboxes()
     wait(3)
